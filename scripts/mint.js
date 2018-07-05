83f6fc2cef108815c70173f710e490ac4d7ac7f6
Devin Finzer <dfinzer2@gmail.com>
2018-07-05 16:54:43 -0400

    Update web3 library

--- scripts/mint.js
+++ scripts/mint.js
@@ -8,35 +8,34 @@ const NETWORK = process.env.NETWORK
 const NUM_PUFFS = 12
 
 if (!MNEMONIC || !INFURA_KEY) {
-  console.error("Please set a mnemonic and infura key.")
-  return
+    console.error("Please set a mnemonic and infura key.")
 }
 
-const ABI = [{ "constant": false, "inputs": [ {  "name": "_to", "type": "address" }, { "name": "_tokenURI", "type": "string" } ], "name": "mintTo", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }]
-
-function callContract(contract, methodName, ...args) {
-    return new Promise((resolve, reject) => {
-      contract[methodName](...args, (err, result) => {
-        if (!err) { 
-          resolve(result);
-        } 
-        else reject(err);
-      });
-    });
-}
+const ABI = [{
+    "constant": false,
+    "inputs": [{ "name": "_to", "type": "address" },
+        { "name": "_tokenURI", "type": "string" }
+    ],
+    "name": "mintTo",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}]
 
 async function main() {
     const web3Instance = new web3(
         new HDWalletProvider(MNEMONIC, `https://${NETWORK}.infura.io/${INFURA_KEY}`)
     )
-    
-    const itemContract = web3Instance.eth.contract(ABI).at(CONTRACT_ADDRESS)
+
+    const itemContract = new web3Instance.eth.Contract(ABI, CONTRACT_ADDRESS, { gasLimit: "1000000" })
+
     for (var i = 0; i < NUM_PUFFS; i++) {
         const puffId = i + 1
-        const result = await callContract(itemContract, "mintTo", OWNER_ADDRESS, `https://cryptopuffs-api.herokuapp.com/api/puff/${puffId}`,
-            { from: OWNER_ADDRESS })
-        console.log(`https://${NETWORK}.etherscan.io/tx/${result}`)
-    }
+        const result = await itemContract.methods.mintTo(OWNER_ADDRESS,
+            `https://cryptopuffs-api.herokuapp.com/api/puff/${puffId}`).send({ from: OWNER_ADDRESS });
+        console.log(result.transactionHash)
+    } 
 }
 
 main()
