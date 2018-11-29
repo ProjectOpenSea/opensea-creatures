97640743218ace515f1b98a1b7924fe54ed13646
Alexander Atallah <alexander.atallah@gmail.com>
2018-11-29 09:35:41 -0500

    fix web3-provider-engine dep and reformat sell.js script

--- scripts/sell.js
+++ scripts/sell.js
@@ -2,10 +2,8 @@ const opensea = require('opensea-js')
 const OpenSeaPort = opensea.OpenSeaPort;
 const Network = opensea.Network;
 
-const HDWalletProvider = require("truffle-hdwallet-provider")
 const MnemonicWalletSubprovider = require('@0x/subproviders').MnemonicWalletSubprovider
 const RPCSubprovider = require('web3-provider-engine/subproviders/rpc')
-const web3 = require('web3')
 const Web3ProviderEngine = require('web3-provider-engine')
 const MNEMONIC = process.env.MNEMONIC
 const INFURA_KEY = process.env.INFURA_KEY
@@ -26,8 +24,8 @@ const INCREMENT_AMOUNT = 10;
 const NUM_PER_INCREMENT = 5;
 const NUM_INCREMENTS = 20;
 
-if (!MNEMONIC || !INFURA_KEY || !NETWORK || !OWNER_ADDRESS) {
-    console.error("Please set a mnemonic, infura key, owner, network, and contract address.")
+if (!MNEMONIC || !INFURA_KEY || !NETWORK || !OWNER_ADDRESS || !FACTORY_CONTRACT_ADDRESS) {
+    console.error("Please set a mnemonic, infura key, owner, network, and factory contract address.")
     return
 }
 const BASE_DERIVATION_PATH = `44'/60'/0'/0`;
@@ -46,27 +44,36 @@ const seaport = new OpenSeaPort(providerEngine, {
 }, (arg) => console.log(arg))
 
 async function main() {
-    if (FACTORY_CONTRACT_ADDRESS) {
 
-        // Example: many declining Dutch auction.
-        console.log("Creating dutch auctions...")
-        for (var i = 0; i < NUM_DUTCH_AUCTIONS; i++) {
-            const expirationTime = Math.round(Date.now() / 1000 + 60 * 60 * 24)
-            const sellOrder = await seaport.createSellOrder({ tokenId: DUTCH_AUCTION_OPTION_ID, tokenAddress: FACTORY_CONTRACT_ADDRESS, accountAddress: OWNER_ADDRESS, 
-                startAmount: DUTCH_AUCTION_START_AMOUNT, endAmount: DUTCH_AUCTION_END_AMOUNT, expirationTime: expirationTime })
-            console.log(sellOrder)
-        }
-
-        // Example: many fixed price auctions.
-        console.log("Creating fixed price auctions...")
-        for (var i = 0; i < NUM_FIXED_PRICE_AUCTIONS; i++) {
-            const sellOrder = await seaport.createSellOrder({ tokenId: FIXED_PRICE_OPTION_ID, tokenAddress: FACTORY_CONTRACT_ADDRESS, accountAddress: OWNER_ADDRESS, 
-                startAmount: FIXED_PRICE })
-            console.log(sellOrder)
-        }
-
-        // TODO: Incremental prices example.
+    // Example: many fixed price auctions.
+    console.log("Creating fixed price auctions...")
+    for (var i = 0; i < NUM_FIXED_PRICE_AUCTIONS; i++) {
+        const sellOrder = await seaport.createSellOrder({
+            tokenId: FIXED_PRICE_OPTION_ID,
+            tokenAddress: FACTORY_CONTRACT_ADDRESS,
+            accountAddress: OWNER_ADDRESS,
+            startAmount: FIXED_PRICE
+        })
+        console.log(`Successfully made fixed-price sell order! ${sellOrder.asset.openseaLink}\n`)
     }
+
+    // Example: many declining Dutch auction.
+    console.log("Creating dutch auctions...")
+    for (var i = 0; i < NUM_DUTCH_AUCTIONS; i++) {
+        // Expire one day from now
+        const expirationTime = Math.round(Date.now() / 1000 + 60 * 60 * 24)
+        const sellOrder = await seaport.createSellOrder({
+            tokenId: DUTCH_AUCTION_OPTION_ID,
+            tokenAddress: FACTORY_CONTRACT_ADDRESS,
+            accountAddress: OWNER_ADDRESS, 
+            startAmount: DUTCH_AUCTION_START_AMOUNT,
+            endAmount: DUTCH_AUCTION_END_AMOUNT,
+            expirationTime: expirationTime
+        })
+        console.log(`Successfully made dutch-auction sell order! ${sellOrder.asset.openseaLink}\n`)
+    }
+
+    // TODO: Incremental prices example.
 }
 
 main()
