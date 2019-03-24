1b029989fdebc354f3be031a4f44325dbb5fa517
Devin Finzer <dfinzer2@gmail.com>
2019-03-24 16:29:27 -0400

    Revert "Change public to external"
    This reverts commit 252706291763830a9f1a8fc3388dbe06c8b9281d.

--- contracts/CreatureFactory.sol
+++ contracts/CreatureFactory.sol
@@ -42,15 +42,15 @@ contract CreatureFactory is Factory, Ownable {
     return "CPF";
   }
 
-  function supportsFactoryInterface() external view returns (bool) {
+  function supportsFactoryInterface() public view returns (bool) {
     return true;
   }
 
-  function numOptions() external view returns (uint256) {
+  function numOptions() public view returns (uint256) {
     return NUM_OPTIONS;
   }
   
-  function mint(uint256 _optionId, address _toAddress) external {
+  function mint(uint256 _optionId, address _toAddress) public {
     // Must be sent from the owner proxy or owner.
     ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
     assert(address(proxyRegistry.proxies(owner())) == msg.sender || owner() == msg.sender || msg.sender == lootBoxNftAddress);
@@ -69,7 +69,7 @@ contract CreatureFactory is Factory, Ownable {
     } 
   }
 
-  function canMint(uint256 _optionId) external view returns (bool) {
+  function canMint(uint256 _optionId) public view returns (bool) {
     if (_optionId >= NUM_OPTIONS) {
       return false;
     }
@@ -100,7 +100,7 @@ contract CreatureFactory is Factory, Ownable {
    * Hack to get things to work automatically on OpenSea.
    * Use transferFrom so the frontend doesn't have to worry about different method names.
    */
-  function transferFrom(address _from, address _to, uint256 _tokenId) external {
+  function transferFrom(address _from, address _to, uint256 _tokenId) public {
     mint(_tokenId, _to);
   }
 
@@ -112,7 +112,7 @@ contract CreatureFactory is Factory, Ownable {
     address _owner,
     address _operator
   )
-    external
+    public
     view
     returns (bool)
   {
@@ -132,7 +132,7 @@ contract CreatureFactory is Factory, Ownable {
    * Hack to get things to work automatically on OpenSea.
    * Use isApprovedForAll so the frontend doesn't have to worry about different method names.
    */
-  function ownerOf(uint256 _tokenId) external view returns (address _owner) {
+  function ownerOf(uint256 _tokenId) public view returns (address _owner) {
     return owner();
   }
 }
