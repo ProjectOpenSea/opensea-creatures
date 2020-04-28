06aa68d7213bda690cef08b18fed5d8c7e7a2761
Beasta <>
2020-04-28 10:08:41 -0700

    Implement eslint autocorrect for semicolons

--- scripts/mint.js
+++ scripts/mint.js
@@ -61,7 +61,7 @@ async function main() {
 
         // Creatures issued directly to the owner.
         for (var i = 0; i < NUM_CREATURES; i++) {
-            const result = await nftContract.methods.mintTo(OWNER_ADDRESS).send({ from: OWNER_ADDRESS });
+            const result = await nftContract.methods.mintTo(OWNER_ADDRESS).send({ from: OWNER_ADDRESS })
             console.log("Minted creature. Transaction: " + result.transactionHash)
         }
     } else if (FACTORY_CONTRACT_ADDRESS) {
@@ -69,13 +69,13 @@ async function main() {
 
         // Creatures issued directly to the owner.
         for (var i = 0; i < NUM_CREATURES; i++) {
-            const result = await factoryContract.methods.mint(DEFAULT_OPTION_ID, OWNER_ADDRESS).send({ from: OWNER_ADDRESS });
+            const result = await factoryContract.methods.mint(DEFAULT_OPTION_ID, OWNER_ADDRESS).send({ from: OWNER_ADDRESS })
             console.log("Minted creature. Transaction: " + result.transactionHash)
         }
 
         // Lootboxes issued directly to the owner.
         for (var i = 0; i < NUM_LOOTBOXES; i++) {
-            const result = await factoryContract.methods.mint(LOOTBOX_OPTION_ID, OWNER_ADDRESS).send({ from: OWNER_ADDRESS });
+            const result = await factoryContract.methods.mint(LOOTBOX_OPTION_ID, OWNER_ADDRESS).send({ from: OWNER_ADDRESS })
             console.log("Minted lootbox. Transaction: " + result.transactionHash)
         }
     } else {
