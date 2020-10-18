98f7e427df3de5656e548ed7cca488c2cebb7e83
Alexander Atallah <alexander.atallah@gmail.com>
2020-10-18 11:12:42 -0400

    fix gas issues

--- truffle.js
+++ truffle.js
@@ -3,12 +3,13 @@ const HDWalletProvider = require("truffle-hdwallet-provider");
 const MNEMONIC = process.env.MNEMONIC;
 const INFURA_KEY = process.env.INFURA_KEY;
 
-const needsInfura = process.env.npm_config_argv &&
-      (process.env.npm_config_argv.includes('rinkeby') ||
-       process.env.npm_config_argv.includes('live'));
+const needsInfura =
+  process.env.npm_config_argv &&
+  (process.env.npm_config_argv.includes("rinkeby") ||
+    process.env.npm_config_argv.includes("live"));
 
 if ((!MNEMONIC || !INFURA_KEY) && needsInfura) {
-  console.error('Please set a mnemonic and infura key.');
+  console.error("Please set a mnemonic and infura key.");
   process.exit(0);
 }
 
@@ -17,41 +18,41 @@ module.exports = {
     development: {
       host: "localhost",
       port: 7545,
-      gas: 4600000,
-      network_id: "*" // Match any network id
+      gas: 5000000,
+      network_id: "*", // Match any network id
     },
     rinkeby: {
-      provider: function() {
+      provider: function () {
         return new HDWalletProvider(
           MNEMONIC,
           "https://rinkeby.infura.io/v3/" + INFURA_KEY
         );
       },
       network_id: "*",
-      networkCheckTimeout: 10000000
+      networkCheckTimeout: 10000000,
     },
     live: {
       network_id: 1,
-      provider: function() {
+      provider: function () {
         return new HDWalletProvider(
           MNEMONIC,
           "https://mainnet.infura.io/v3/" + INFURA_KEY
         );
       },
       gas: 4000000,
-      gasPrice: 5000000000
-    }
+      gasPrice: 5000000000,
+    },
   },
   mocha: {
-    reporter: 'eth-gas-reporter',
-    reporterOptions : {
-      currency: 'USD',
-      gasPrice: 2
-    }
+    reporter: "eth-gas-reporter",
+    reporterOptions: {
+      currency: "USD",
+      gasPrice: 2,
+    },
   },
   compilers: {
     solc: {
-      version: "^0.5.0"
-    }
-  }
+      version: "^0.5.0",
+    },
+  },
 };
